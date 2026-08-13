60f8b39c45d18a888cd7684a2b9cadbf3d3c1c3c
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-08-13 15:38:38 +0100

    updated 8th activity

--- index2.js
+++ index2.js
@@ -1,5 +1,7 @@
-
-
+// Activity 8: Reusable Machines
+/**
+  The program will have two functions. The first function will calculate the tip using the bill amount and tip percentage. The second function will display a formatted message showing the bill and calculated tip
+ */
 
 function calculateTip(bill, percentage) {
     let tip = bill * (percentage / 100);
@@ -11,10 +13,11 @@ function displayMessage(bill, tip) {
     console.log(`Tip: ₦${tip}`);
 }
 
-// Test values
 let billAmount = 10000;
 let tipPercentage = 10;
 
 let tipAmount = calculateTip(billAmount, tipPercentage);
 
 displayMessage(billAmount, tipAmount);
+
+
